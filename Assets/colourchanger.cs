8b8130c018755beabeecf6bb0851fa84b420985d
Kyle Marais <kyledonaldmarais@gmail.com>
2020-10-16 13:20:41 +0200

    Added Colour changer script
    Press 'R' to change to red, and press 'G' to change to green

---
+++ Assets/colourchanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colourchanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sphere;
+    public Material red;
+    public Material green;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            sphere.GetComponent<MeshRenderer>().material = red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            sphere.GetComponent<MeshRenderer>().material = green;
+        }
+    }
+}
